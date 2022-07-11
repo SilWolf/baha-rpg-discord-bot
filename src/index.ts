2b5a30543a3c8c611cc462872a5e575eee51f495
SilWolf <silwolf1121@gmail.com>
2022-07-11 21:00:23 +0800

    feat: install pm2

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,8 @@ const main = async () => {
     }
   })
 
-  client.login(token)
-
   // Start the scheduler
-  const job = schedule.scheduleJob(
+  schedule.scheduleJob(
     '1,6,11,16,21,26,31,36,41,46,51,56 * * * *',
     async () => {
       const posts = await getNewPosts()
@@ -69,6 +67,11 @@ const main = async () => {
       })
     }
   )
+
+  // Initialize first posts
+  await getNewPosts()
+
+  client.login(token)
 }
 
 export default main()
