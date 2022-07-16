1125351095565314c1f94746fdd539d3b112bf86
SilWolf <silwolf1121@gmail.com>
2022-07-16 20:52:28 +0800

    chore: rewrite baha post.api

--- src/services/baha/post.api.ts
+++ src/services/baha/post.api.ts
@@ -1,76 +1,75 @@
 import { BahaPost, BahaPostRaw } from './types/bahaPost.type'
 import api, { BahaAPIResponse } from '.'
-import { postLogin } from './auth.api'
 
 let cachedlastPostId: string | undefined
 
-export const getPosts = async (lastPostId?: string): Promise<BahaPost[]> => {
-  const fn = () =>
-    api
-      .get<BahaAPIResponse<{ lastSn: string; postList: BahaPostRaw[][] }>>(
-        '/guild/v1/post_list.php',
-        {
-          params: {
-            gsn: 3014,
-          },
-        }
-      )
-      .then((res) => {
-        if (!res.data.data) {
-          return []
-        }
+export const getPostsRawResponse = async (lastSn?: string) =>
+  api
+    .get<BahaAPIResponse<{ lastSn: string; postList: BahaPostRaw[][] }>>(
+      '/guild/v1/post_list.php',
+      {
+        params: {
+          gsn: 3014,
+          last: lastSn,
+        },
+      }
+    )
+    .then((res) => res.data.data)
 
-        const rawPosts = res.data.data.postList ?? []
-        if (rawPosts.length === 0) {
-          return []
-        }
+export const getPosts = async (lastSn?: string): Promise<BahaPost[]> =>
+  getPostsRawResponse(lastSn).then((res) => {
+    if (!res) {
+      return []
+    }
 
-        const posts = []
+    const rawPosts = res.postList ?? []
+    if (rawPosts.length === 0) {
+      return []
+    }
 
-        for (let i = 0; i < rawPosts.length; i += 1) {
-          const rawPost = rawPosts[i][0]
-          if (rawPost) {
-            if (lastPostId && lastPostId <= rawPost?.id) {
-              break
-            }
+    const posts = []
 
-            posts.push({
-              id: rawPost.id,
-              publisher: rawPost.publisher,
-              title: rawPost.content
-                .split('\n')
-                .filter((content) => !!content)[0],
-              content: rawPost.content,
-              ctime: rawPost.ctime,
-              to: rawPost.to,
-              urlPreview: Array.isArray(rawPost.urlPreview)
-                ? undefined
-                : rawPost.urlPreview,
-              bahaUrl: `https://guild.gamer.com.tw/post_detail.php?gsn=${rawPost.to.gsn}&sn=${rawPost.id}`,
-              eternalUrl: `https://www.isaka.idv.tw/History/viewMsg.html?sn=${rawPost.id}`,
-              ctimeDate: new Date(rawPost.ctime),
-            })
-          }
-        }
+    for (let i = 0; i < rawPosts.length; i += 1) {
+      const rawPost = rawPosts[i][0]
+      if (rawPost) {
+        posts.push({
+          id: rawPost.id,
+          publisher: rawPost.publisher,
+          title: rawPost.content
+            .split('\n')
+            .filter((contentSector: string) => !!contentSector)[0],
+          content: rawPost.content,
+          ctime: rawPost.ctime,
+          to: rawPost.to,
+          urlPreview: Array.isArray(rawPost.urlPreview)
+            ? undefined
+            : rawPost.urlPreview,
+          bahaUrl: `https://guild.gamer.com.tw/post_detail.php?gsn=${rawPost.to.gsn}&sn=${rawPost.id}`,
+          eternalUrl: `https://www.isaka.idv.tw/History/viewMsg.html?sn=${rawPost.id}`,
+          ctimeDate: new Date(rawPost.ctime),
+        })
+      }
+    }
 
-        if (posts.length > 0) {
-          cachedlastPostId = posts[0].id
-        }
+    if (posts.length > 0) {
+      cachedlastPostId = posts[0].id
+    }
 
-        return posts
-      })
-
-  try {
-    const posts = await fn()
     return posts
-  } catch (e) {
-    // await postLogin()
-    console.log(e)
-    // const posts = await fn()
-    return []
-  }
-}
+  })
 
-export const getNewPosts = (): Promise<BahaPost[]> => getPosts(cachedlastPostId)
+export const getNewPosts = async (): Promise<BahaPost[]> => {
+  const posts = await getPosts().then((_posts) =>
+    cachedlastPostId
+      ? _posts.filter((post) => post.id > (cachedlastPostId as string))
+      : _posts
+  )
+
+  if (posts.length > 0) {
+    cachedlastPostId = posts[0].id
+  }
+
+  return posts
+}
 
 export default {}
